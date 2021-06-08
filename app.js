0fe34e24b414a264713d5b36c94c8d062ba6de63
Isaacdev-lab <zikoinnovations@gmail.com>
2021-06-08 13:15:28 +0100

    Editing the DOM

--- app.js
+++ app.js
@@ -16,6 +16,10 @@
 
 var titles = document.getElementsByClassName('title');
 
+Array.from(titles).forEach(function(item){
+  item.textContent = 'Isaac';
+});
+
 // console.log(Array.isArray(titles));
 // console.log(Array.isArray(Array.from(titles)))
 
@@ -28,4 +32,21 @@ Array.from(titles).forEach(function(item){
 const wrap = document.querySelector('#wrapper');
 console.log(wrap);
 
-console.
+const B2N = document.querySelector('#book-list li:nth-child(2) .name');
+console.log(B2N);
+
+const allBooks = document.querySelectorAll('#book-list li .name');
+console.log(allBooks);
+
+Array.from(titles).forEach(function(item){
+  console.log(item)
+});
+
+// How to change and manipulate text
+
+var books = document.querySelectorAll('#book-list li .name');
+
+Array.from(books).forEach(function(book){
+  book.textContent = 'New Books';
+});
+
